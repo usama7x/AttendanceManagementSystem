363ab392a6a8ca5789695029ce1c9f216a289630
Usama <Usama18@icloud.com>
2020-07-20 15:26:35 +0500

    fixed

--- AttendanceManagementSystem/Controllers/TeacherController.cs
+++ AttendanceManagementSystem/Controllers/TeacherController.cs
@@ -37,20 +37,18 @@ namespace AttendanceManagementSystem.Controllers
             var result = await userManager.CreateAsync(teacher, teacher.Password);
             if (result.Succeeded)
             {
-                await userManager.AddToRoleAsync(teacher, "teacher");
                 return RedirectToAction("Index");
             } else
             {
                 return NotFound();
             }
-        }
+                
+           
 
-        public IActionResult Delete(int id)
-        {
-            var teacher = context.Teachers.Find(id);
-            context.Teachers.Remove(teacher);
-            context.SaveChanges();
-            return RedirectToAction("Index");
+
+
+
+
         }
     }
 }
